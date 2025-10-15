947c00209d28dbc624e8bbdbf586e8beddf69fab
ChayaRubin <Hand4d26@mbjcomp.org.il>
2025-10-15 12:34:12 +0300

    Fix NavBar.jsx casing

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext.jsx';
-import styles from './Navbar.module.css';
+import styles from './NavBar.module.css';
 
 export default function Navbar() {
   const { isAuthenticated, logout } = useAuth();
