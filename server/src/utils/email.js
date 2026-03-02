7ea118174c38f9ad0ecd4058cbe03785502148c1
ChayaRubin <Hand4d26@mbjcomp.org.il>
2026-03-03 01:53:57 +0200

    fixing email sending code

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -2,11 +2,16 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // VERY important
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS, // MUST be app password
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export const sendEmail = async ({ from, to, subject, html, attachments }) => {
