cab6596f8c55eccb024dfe5ab7a8167a5418d1a7
ChayaRubin <Hand4d26@mbjcomp.org.il>
2026-04-19 23:12:30 +0300

    changed footer

--- client/src/layouts/PublicLayout/PublicLayout.jsx
+++ client/src/layouts/PublicLayout/PublicLayout.jsx
@@ -12,22 +12,23 @@ export default function PublicLayout() {
         <Outlet />
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={styles.footer}>       
+        {/* <a 
+          // href="https://pixel-perfect-2b1f2fe2.base44.app/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.footerLink}
+        > */}
+          © 2025 Created and Designed By CT websites
+        {/* </a> */}
+        <br/>
         <a 
           href="mailto:ct3227200@gmail.com?subject=Hello&body=Hi%20there!" 
           className={styles.footerLink}
         >
-          Contact Me - ct3227200@gmail.com
+          ct3227200@gmail.com
         </a>
-        <br />
-        <a 
-          href="https://pixel-perfect-2b1f2fe2.base44.app/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className={styles.footerLink}
-        >
-          © 2025 Created and Designed By CT websites
-        </a>
+         <br />
       </footer>
     </div>
   );
